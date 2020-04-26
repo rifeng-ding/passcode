01287fbd8d2a6002349d73eb0a7d3785a24577f0
rifeng-ding <rifeng.ding@gmail.com>
2020-04-26 09:42:40 -0400

    Initial Commit

---
+++ passcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passcode
+//
+//  Created by Rifeng Ding on 2020-04-26.
+//  Copyright © 2020 Rifeng Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
